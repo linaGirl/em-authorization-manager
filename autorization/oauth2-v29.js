d2562dfcabd131a3cc4dd7129c3113fde66027da
Michael van der Weg <michael@vanderweg.ch>
2013-08-15 14:31:09 +0200

    initial

---
+++ autorization/oauth2-v29.js
@@ -0,0 +1,19 @@
+
+
+	var   Class = require( "ee-class" )
+		, log 	= require( "ee-log" );
+
+
+
+	module.exports = new Class( {
+
+		init: function( options ){
+
+		}
+
+
+
+		, authorize: function( request, response, callback ){
+
+		}
+	} );
